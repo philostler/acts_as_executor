c2f93d98c649ea486b95b366e628601ac72e44d0
Phil Ostler <philostler@gmail.com>
2011-05-05 23:58:37 +0100

    Additional requires and logging logic.

--- lib/acts_as_executor.rb
+++ lib/acts_as_executor.rb
@@ -1,4 +1,19 @@
 require "acts_as_executor/model/executor/base"
+require "acts_as_executor/model/executor/factory"
 require "acts_as_executor/model/executor/instance_methods"
+require "acts_as_executor/model/executor/kinds"
+require "acts_as_executor/model/executor/validations"
 
 ActiveRecord::Base.send :extend, ActsAsExecutor::Model::Executor::Base
+
+module ActsAsExecutor
+  def self.log
+    logger
+  end
+  def self.logger
+    @@logger ||= Rails.logger
+  end
+  def self.logger= logger
+    @@logger = logger
+  end
+end
