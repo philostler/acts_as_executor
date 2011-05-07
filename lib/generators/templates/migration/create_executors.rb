0118376bf33b5c04ae132869031b672949278fca
Phil Ostler <philostler@gmail.com>
2011-05-07 13:48:10 +0100

    Additional task table.

--- lib/generators/templates/migration/create_executors.rb
+++ lib/generators/templates/migration/create_executors.rb
@@ -6,9 +6,20 @@ class Create<%= class_name.pluralize %> < ActiveRecord::Migration
       t.integer  :size
       t.timestamps
     end
+
+    create_table :<%= singular_table_name %>_tasks do |t|
+      t.integer  :executor_id
+      t.string   :clazz
+      t.string   :schedule
+      t.integer  :start
+      t.integer  :every
+      t.string   :unit
+      t.timestamps
+    end
   end
 
   def self.down
     drop_table :<%= table_name %>
+    drop_table :<%= singular_table_name %>_tasks
   end
 end
