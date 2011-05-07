a69de705a5c33b5dad196755c52e2ef5b633fcd7
Phil Ostler <philostler@gmail.com>
2011-05-07 13:44:57 +0100

    Additional require and extends for tasks.

--- lib/acts_as_executor.rb
+++ lib/acts_as_executor.rb
@@ -1,10 +1,18 @@
+require "acts_as_executor/model/executor/associations"
 require "acts_as_executor/model/executor/base"
 require "acts_as_executor/model/executor/factory"
 require "acts_as_executor/model/executor/instance_methods"
 require "acts_as_executor/model/executor/kinds"
 require "acts_as_executor/model/executor/validations"
 
+require "acts_as_executor/model/task/associations"
+require "acts_as_executor/model/task/base"
+require "acts_as_executor/model/task/validations"
+
+require "acts_as_executor/validators/class_exists_validator"
+
 ActiveRecord::Base.send :extend, ActsAsExecutor::Model::Executor::Base
+ActiveRecord::Base.send :extend, ActsAsExecutor::Model::Task::Base
 
 module ActsAsExecutor
   def self.log
