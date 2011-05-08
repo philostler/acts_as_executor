90866da33e17bcf39d5a60a1abdfcaaf40d4993f
Phil Ostler <philostler@gmail.com>
2011-05-08 13:40:01 +0100

    Refactored and enabled validations.

--- lib/acts_as_executor/model/task/validations.rb
+++ lib/acts_as_executor/model/task/validations.rb
@@ -5,10 +5,10 @@ module ActsAsExecutor
         def self.included base
           base.validates :executor_id, :presence => true
           base.validates :clazz, :presence => true, :class_exists => true
-#          base.validates :schedule, :inclusion => { :in => ActsAsExecutor::Task::Schedules::ALL }, :if => "executor and ActsAsExecutor::Executor::Kinds::ALL_SCHEDULED.include? executor.kind and schedule"
-#          base.validates :start, :numericality => { :only_integer => true }, :length => { :minimum => 0 }, :if => "executor and ActsAsExecutor::Executor::Kinds::ALL_SCHEDULED.include? executor.kind and ActsAsExecutor::Task::Schedules::REQUIRING_START.include? schedule"
-#          base.validates :every, :numericality => { :only_integer => true }, :length => { :minimum => 1 }, :if => "executor and ActsAsExecutor::Executor::Kinds::ALL_SCHEDULED.include? executor.kind and ActsAsExecutor::Task::Schedules::REQUIRING_EVERY.include? schedule"
-#          base.validates :unit, :inclusion => { :in => ActsAsExecutor::Task::Units::ALL }, :if => "executor and ActsAsExecutor::Executor::Kinds::ALL_SCHEDULED.include? executor.kind and ActsAsExecutor::Task::Schedules::REQUIRING_UNIT.include? schedule"
+          base.validates :schedule, :inclusion => { :in => ActsAsExecutor::Model::Task::Schedules::ALL }, :if => "executor and executor.schedulable?"
+          base.validates :start, :numericality => { :only_integer => true }, :length => { :minimum => 0 }, :if => "executor and executor.schedulable?"
+          base.validates :every, :numericality => { :only_integer => true }, :length => { :minimum => 1 }, :if => "executor and executor.schedulable? and schedule != ActsAsExecutor::Model::Task::Schedules::ONE_SHOT"
+          base.validates :unit, :inclusion => { :in => ActsAsExecutor::Model::Task::Units::ALL }, :if => "executor and executor.schedulable?"
         end
       end
     end
