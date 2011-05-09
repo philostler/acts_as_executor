0c3fb79d3c3ee820a9a947190aab25e7e05a94ff
Phil Ostler <philostler@gmail.com>
2011-05-10 00:56:27 +0100

    Associated tasks scheduled at startup.

--- lib/acts_as_executor/model/executor/instance_methods.rb
+++ lib/acts_as_executor/model/executor/instance_methods.rb
@@ -22,11 +22,44 @@ module ActsAsExecutor
           @@executors[id] = executor
         end
 
+        def submit task
+          clazz = task.clazz.constantize.new
+
+          begin
+            case self.executor
+              when Java::java.util.concurrent.ScheduledExecutorService
+                unit = Java::java.util.concurrent.TimeUnit.value_of(task.unit.upcase)
+                case task.schedule
+                  when ActsAsExecutor::Model::Task::Schedules::ONE_SHOT
+                    self.executor.schedule clazz, task.start, unit
+                  when ActsAsExecutor::Model::Task::Schedules::FIXED_DELAY
+                    self.executor.schedule_with_fixed_delay clazz, task.start, task.every, unit
+                  when ActsAsExecutor::Model::Task::Schedules::FIXED_RATE
+                    self.executor.schedule_at_fixed_rate clazz, task.start, task.every, unit
+                end
+              when Java::java.util.concurrent.ExecutorService
+                self.executor.submit clazz
+            end
+          rescue Java::java.lang.NullPointerException
+            ActsAsExecutor.log.error "Executor \"" + name + "\" attaching task \"" + task.clazz + "\" threw a NullPointerException"
+          rescue Java::java.lang.IllegalArgumentException
+            ActsAsExecutor.log.error "Executor \"" + name + "\" attaching task \"" + task.clazz + "\" threw a IllegalArgumentException"
+          rescue Java::java.util.concurrent.RejectedExecutionException
+            ActsAsExecutor.log.error "Executor \"" + name + "\" attaching task \"" + task.clazz + "\" threw a RejectedExecutionException"
+          end
+        end
+        
         def startup
           unless self.executor
             ActsAsExecutor.log.debug "Executor \"" + name + "\" starting up..."
             self.executor = ActsAsExecutor::Model::Executor::Factory.create kind, size
             ActsAsExecutor.log.info "Executor \"" + name + "\" has completed startup"
+            
+            ActsAsExecutor.log.debug "Executor \"" + name + "\" attaching tasks..."
+            tasks.each do |t|
+              submit t
+            end
+            ActsAsExecutor.log.info "Executor \"" + name + "\" has completed attaching tasks"
           end
         end
 
