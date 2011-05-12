3a72195391e2001a933891ad6071f81342ed1e13
Phil Ostler <philostler@gmail.com>
2011-05-12 17:10:43 +0100

    Logging to Rails directly and improved if on request property set.

--- lib/acts_as_executor/tasks/http/abstract_http_task.rb
+++ lib/acts_as_executor/tasks/http/abstract_http_task.rb
@@ -1,1 +1,31 @@
-module ActsAsExecutor  module Tasks    module Http      # Abstract HTTP task that establishes a connection to a specified URI      class AbstractHttpTask        include Java::java.util.concurrent.Callable        attr_accessor :uri, :etag, :logger        # Returns a HttpURLConnection[http://download.oracle.com/javase/6/docs/api/java/net/HttpURLConnection.html] or nil if an exception occurred        def call          connection = nil          begin                      uri = Java::java.net.URL.new @uri            connection = uri.open_connection            connection.set_request_property "If-None-Match", @etag ||= ""            connection.connect          rescue Java::java.net.MalformedURLException            @logger.error { "Unable to attempt connection to URI \"" + @uri + "\". Unknown protocol" } unless @logger.nil?          rescue Java::java.io.IOException            @logger.warn { "Unable to open connection to URI \"" + @uri + "\"" } unless @logger.nil?          rescue Java::java.net.SocketTimeoutException            @logger.warn { "Unable to establish connection to URI \"" + @uri + "\". Timeout has expired" } unless @logger.nil?          end          return connection        end      end    end  endend
+module ActsAsExecutor
+  module Tasks
+    module Http
+      # Abstract HTTP task that establishes a connection to a specified URI
+      class AbstractHttpTask
+        include Java::java.util.concurrent.Callable
+
+        attr_accessor :uri, :etag
+
+        # Returns a HttpURLConnection[http://download.oracle.com/javase/6/docs/api/java/net/HttpURLConnection.html] or nil if an exception occurred
+        def get_connection
+          connection = nil
+          begin          
+            uri = Java::java.net.URL.new @uri
+
+            connection = uri.open_connection
+            connection.set_request_property("If-None-Match", @eta) if @etag
+            connection.connect
+          rescue Java::java.net.MalformedURLException
+            Rails.logger.error "Unable to attempt connection to URI \"" + @uri + "\". Unknown protocol"
+          rescue Java::java.io.IOException
+            Rails.logger.warn "Unable to open connection to URI \"" + @uri + "\""
+          rescue Java::java.net.SocketTimeoutException
+            Rails.logger.warn "Unable to establish connection to URI \"" + @uri + "\". Timeout has expired"
+          end
+          return connection
+        end
+      end
+    end
+  end
+end
