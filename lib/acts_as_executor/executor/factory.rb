9af0b9c172faf528d41f0a895adf3f1b8ee1d8de
Phil Ostler <philostler@gmail.com>
2011-06-01 23:04:00 +0100

    Added non-scheduled factory cases.

--- lib/acts_as_executor/executor/factory.rb
+++ lib/acts_as_executor/executor/factory.rb
@@ -3,12 +3,25 @@ module ActsAsExecutor
     class Factory
       def self.create kind, size = nil
         case kind
+          when ActsAsExecutor::Executor::Kinds::CACHED
+            return Java::java.util.concurrent.Executors.new_cached_thread_pool
+
+          when ActsAsExecutor::Executor::Kinds::FIXED
+            raise TypeError, "size cannot be nil" unless size
+            raise ArgumentError, "size must be larger than 0" unless size.to_i > 0
+            return Java::java.util.concurrent.Executors.new_fixed_thread_pool size.to_i
+
+          when ActsAsExecutor::Executor::Kinds::SINGLE
+            return Java::java.util.concurrent.Executors.new_single_thread_executor
+
           when ActsAsExecutor::Executor::Kinds::SCHEDULED
             raise TypeError, "size cannot be nil" unless size
             raise ArgumentError, "size must be larger than 0" unless size.to_i > 0
             return Java::java.util.concurrent.Executors.new_scheduled_thread_pool size.to_i
+
           when ActsAsExecutor::Executor::Kinds::SINGLE_SCHEDULED
             return Java::java.util.concurrent.Executors.new_single_thread_scheduled_executor
+
           else
             return nil
         end
