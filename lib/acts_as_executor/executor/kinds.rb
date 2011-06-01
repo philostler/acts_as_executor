7281208e4d1c805f15745e1b412aaa664bd70c12
Phil Ostler <philostler@gmail.com>
2011-06-01 23:03:12 +0100

    Added non-scheduled kinds.

--- lib/acts_as_executor/executor/kinds.rb
+++ lib/acts_as_executor/executor/kinds.rb
@@ -1,12 +1,16 @@
 module ActsAsExecutor
   module Executor
     class Kinds
+      CACHED = "cached"
+      FIXED = "fixed"
+      SINGLE = "single"
+
       SCHEDULED = "scheduled"
       SINGLE_SCHEDULED = "single_scheduled"
 
-      ALL = [ SCHEDULED, SINGLE_SCHEDULED ]
+      ALL = [ CACHED, FIXED, SINGLE, SCHEDULED, SINGLE_SCHEDULED ]
       ALL_SCHEDULED = [ SCHEDULED, SINGLE_SCHEDULED ]
-      REQUIRING_SIZE = [ SCHEDULED ]
+      REQUIRING_SIZE = [ FIXED, SCHEDULED ]
     end
   end
 end
