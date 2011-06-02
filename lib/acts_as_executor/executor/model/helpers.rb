701fdaffd1e2d8d24dffb70e3554a994e461ed27
Phil Ostler <philostler@gmail.com>
2011-06-02 12:48:57 +0100

    Helper methods for when to startup and shutdown.

--- lib/acts_as_executor/executor/model/helpers.rb
+++ lib/acts_as_executor/executor/model/helpers.rb
@@ -2,6 +2,20 @@ module ActsAsExecutor
   module Executor
     module Model
       module Helpers
+        def startup_now?
+          unless self.executor && ActsAsExecutor.rails_startup?
+            return true
+          end
+          return false
+        end
+
+        def shutdown_now?
+          if self.executor && ActsAsExecutor.rails_startup?
+            return true
+          end
+          return false
+        end
+
         def schedulable?
           ActsAsExecutor::Executor::Kinds::ALL_SCHEDULED.include? kind
         end
