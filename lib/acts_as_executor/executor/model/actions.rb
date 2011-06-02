3d2429284d0f6a32bc28871e0372c10d4f351254
Phil Ostler <philostler@gmail.com>
2011-06-02 12:50:32 +0100

    Use helper methods to simplify logic make executor start and shutdown automatically even when created after startup.

--- lib/acts_as_executor/executor/model/actions.rb
+++ lib/acts_as_executor/executor/model/actions.rb
@@ -3,36 +3,34 @@ module ActsAsExecutor
     module Model
       module Actions
         def self.included base
-          base.after_find :startup, :if => "ActsAsExecutor.rails_startup?"
-          base.after_destroy :shutdown
+          base.after_find :startup, :if => :startup_now?
+          base.after_save :startup, :if => :startup_now?
+
+          base.after_destroy :shutdown, :if => :shutdown_now?
         end
-        
+
         def startup
-          unless self.executor
-            ActsAsExecutor.log.debug "Executor \"" + name + "\" starting up..."
-            self.executor = ActsAsExecutor::Executor::Factory.create kind, size
-            ActsAsExecutor.log.info "Executor \"" + name + "\" has completed startup"
-            
-            ActsAsExecutor.log.debug "Executor \"" + name + "\" attaching tasks..."
-            tasks.each do |t|
-              submit t
-            end
-            ActsAsExecutor.log.info "Executor \"" + name + "\" has completed attaching tasks"
+          ActsAsExecutor.log.debug "Executor \"" + name + "\" starting up..."
+          self.executor = ActsAsExecutor::Executor::Factory.create kind, size
+          ActsAsExecutor.log.info "Executor \"" + name + "\" has completed startup"
+          
+          ActsAsExecutor.log.debug "Executor \"" + name + "\" attaching tasks..."
+          tasks.each do |t|
+            submit t
           end
+          ActsAsExecutor.log.info "Executor \"" + name + "\" has completed attaching tasks"
         end
 
         def shutdown
-          if self.executor
-            ActsAsExecutor.log.debug "Executor \"" + name + "\" shutting down..."
-            begin
-              self.executor.shutdown
-            rescue Java::java.lang.RuntimePermission
-              ActsAsExecutor.log.error "Executor \"" + name + "\" has experienced a RuntimePermission error"
-            rescue Java::java.lang.SecurityException
-              ActsAsExecutor.log.error "Executor \"" + name + "\" has experienced a SecurityException error"
-            end
-            ActsAsExecutor.log.info "Executor \"" + name + "\" has completed shutdown"
+          ActsAsExecutor.log.debug "Executor \"" + name + "\" shutting down..."
+          begin
+            self.executor.shutdown
+          rescue Java::java.lang.RuntimePermission
+            ActsAsExecutor.log.error "Executor \"" + name + "\" has experienced a RuntimePermission error"
+          rescue Java::java.lang.SecurityException
+            ActsAsExecutor.log.error "Executor \"" + name + "\" has experienced a SecurityException error"
           end
+          ActsAsExecutor.log.info "Executor \"" + name + "\" has completed shutdown"
         end
 
         def submit task
@@ -50,26 +48,25 @@ module ActsAsExecutor
           end
 
           begin
-            case self.executor
-              when Java::java.util.concurrent.ScheduledExecutorService
-                units = Java::java.util.concurrent.TimeUnit.value_of(task.units.upcase)
-                case task.schedule
-                  when ActsAsExecutor::Task::Schedules::ONE_SHOT
-                    self.executor.schedule clazz, task.start, units
-                  when ActsAsExecutor::Task::Schedules::FIXED_DELAY
-                    self.executor.schedule_with_fixed_delay clazz, task.start, task.every, units
-                  when ActsAsExecutor::Task::Schedules::FIXED_RATE
-                    self.executor.schedule_at_fixed_rate clazz, task.start, task.every, units
-                end
-              when Java::java.util.concurrent.ExecutorService
-                case clazz
-                  when Java::java.util.concurrent.Callable
-                    future_task = ActsAsExecutor::Executor::FutureTask.new clazz
-                  when Java::java.lang.Runnable
-                    future_task = ActsAsExecutor::Executor::FutureTask.new clazz, nil
-                end
-                future_task.task = task
-                self.executor.execute future_task
+            if schedulable?
+              units = Java::java.util.concurrent.TimeUnit.value_of(task.units.upcase)
+              case task.schedule
+                when ActsAsExecutor::Task::Schedules::ONE_SHOT
+                  self.executor.schedule clazz, task.start, units
+                when ActsAsExecutor::Task::Schedules::FIXED_DELAY
+                  self.executor.schedule_with_fixed_delay clazz, task.start, task.every, units
+                when ActsAsExecutor::Task::Schedules::FIXED_RATE
+                  self.executor.schedule_at_fixed_rate clazz, task.start, task.every, units
+              end
+            else
+              case clazz
+                when Java::java.util.concurrent.Callable
+                  future_task = ActsAsExecutor::Executor::FutureTask.new clazz
+                when Java::java.lang.Runnable
+                  future_task = ActsAsExecutor::Executor::FutureTask.new clazz, nil
+              end
+              future_task.task = task
+              self.executor.execute future_task
             end
           rescue Java::java.lang.NullPointerException
             ActsAsExecutor.log.error "Executor \"" + name + "\" attaching task \"" + task.clazz + "\" threw a NullPointerException"
