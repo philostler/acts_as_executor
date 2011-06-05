30afb653fb83cd920a6e0d80f2bb37fa77c042ea
Phil Ostler <philostler@gmail.com>
2011-06-05 22:07:35 +0100

    Additional includes.

--- lib/acts_as_executor/task/model/base.rb
+++ lib/acts_as_executor/task/model/base.rb
@@ -3,8 +3,10 @@ module ActsAsExecutor
     module Model
       module Base
         def acts_as_executor_task
-          send :include, ActsAsExecutor::Task::Model::Associations
           send :include, ActsAsExecutor::Task::Model::Actions
+          send :include, ActsAsExecutor::Task::Model::Associations
+          send :include, ActsAsExecutor::Task::Model::Attributes
+          send :include, ActsAsExecutor::Task::Model::Helpers
           send :include, ActsAsExecutor::Task::Model::Validations
         end
       end
