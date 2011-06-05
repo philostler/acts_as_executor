a59611fec7aafe4a6914f874b5734bff5623d122
Phil Ostler <philostler@gmail.com>
2011-06-05 22:04:38 +0100

    Logic correction.

--- lib/acts_as_executor/executor/model/helpers.rb
+++ lib/acts_as_executor/executor/model/helpers.rb
@@ -3,14 +3,14 @@ module ActsAsExecutor
     module Model
       module Helpers
         def startup_now?
-          unless self.executor && ActsAsExecutor.rails_startup?
+          if self.executor == nil && ActsAsExecutor.rails_startup?
             return true
           end
           return false
         end
 
         def shutdown_now?
-          if self.executor && ActsAsExecutor.rails_startup?
+          if self.executor != nill && ActsAsExecutor.rails_startup?
             return true
           end
           return false
