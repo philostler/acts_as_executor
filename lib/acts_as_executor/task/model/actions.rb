45e4adf3863ea87a9b57d421977e98feed556129
Phil Ostler <philostler@gmail.com>
2011-06-05 22:08:37 +0100

    Submission logic moved into task model.

--- lib/acts_as_executor/task/model/actions.rb
+++ lib/acts_as_executor/task/model/actions.rb
@@ -4,6 +4,28 @@ module ActsAsExecutor
       module Actions
         def self.included base
           base.serialize :arguments, Hash
+
+          base.after_find :execute, :if => :execute_now?
+          base.after_save :execute, :if => :execute_now?
+        end
+
+        def execute
+          begin
+            instance = clazz.constantize.new
+          rescue NameError
+            ActsAsExecutor.log.error "Task creating task could not create class"
+          end
+
+          if arguments
+            arguments.each_pair do |k, v|
+              attribute = "#{k}="
+              instance.send attribute, v if instance.respond_to? attribute
+            end
+          end
+
+          self.future = executor.execute instance, schedule, start, every, units
+          # if there's a ScheduledFuture there is no task to set thus exception being thrown
+          self.future.task = self
         end
       end
     end
