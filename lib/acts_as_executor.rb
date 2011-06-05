d68f0cc190f973a95514b84d4d950a2e1ef397c6
Phil Ostler <philostler@gmail.com>
2011-06-05 22:15:48 +0100

    Updated requires, logger setter and configuration method.

--- lib/acts_as_executor.rb
+++ lib/acts_as_executor.rb
@@ -1,7 +1,7 @@
+require "acts_as_executor/common/future_task"
 require "acts_as_executor/common/units"
 
 require "acts_as_executor/executor/factory"
-require "acts_as_executor/executor/future_task"
 require "acts_as_executor/executor/kinds"
 
 require "acts_as_executor/executor/model/actions"
@@ -15,7 +15,9 @@ require "acts_as_executor/task/schedules"
 
 require "acts_as_executor/task/model/actions"
 require "acts_as_executor/task/model/associations"
+require "acts_as_executor/task/model/attributes"
 require "acts_as_executor/task/model/base"
+require "acts_as_executor/task/model/helpers"
 require "acts_as_executor/task/model/validations"
 
 require "acts_as_executor/validators/class_exists_validator"
@@ -30,6 +32,14 @@ module ActsAsExecutor
   def self.logger
     @@logger ||= Rails.logger
   end
+  def self.logger= logger
+    @@logger = logger
+  end
+
+  def self.configure
+    yield self
+  end
+
   def self.rails_startup?
     File.basename($0) == "rails"
   end
