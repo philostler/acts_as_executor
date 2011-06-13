3ab8efcb02bdb3b1d714cc7b5324e4fe8c062773
Phil Ostler <philostler@gmail.com>
2011-06-14 00:10:18 +0100

    Database connection, schema and model for ActiveRecord testing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,21 @@
-require "active_model"
-require "active_record"
-
 require "acts_as_executor"
 require "acts_as_executor/version"
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "jdbcsqlite3",
+  :database => ":memory:"
+)
+
+ActiveRecord::Schema.define do
+  self.verbose = false
+
+  create_table :executors, :force => true do |t|
+    t.string  :name
+    t.string  :kind
+    t.integer :size
+  end
+end
+
+class Executor < ActiveRecord::Base
+  acts_as_executor
+end
