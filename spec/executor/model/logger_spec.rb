c94d23eff47a34cbc4cf5bf716b4ce3a5452a972
Phil Ostler <philostler@gmail.com>
2011-06-16 23:17:28 +0100

    Initial commit. #2

---
+++ spec/executor/model/logger_spec.rb
@@ -0,0 +1,39 @@
+require "spec_helper"
+
+describe ActsAsExecutor::Executor::Model::Logger do
+  describe "#logger" do
+    context "default logger" do
+      it "should return Rails.logger" do
+        double_logger = double "Logger"
+        Rails.logger = double_logger
+
+        @model = Executor.new
+        @model.log.should eq double_logger
+        @model.logger.should eq double_logger
+      end
+    end
+    context "custom logger" do
+      it "should return custom logger" do
+        double_logger = double "Logger"
+
+        ExecutorWithoutExtension.acts_as_executor :logger => double_logger
+
+        @model = ExecutorWithoutExtension.new
+        @model.log.should eq double_logger
+        @model.logger.should eq double_logger
+      end
+    end
+    context "nil logger" do
+      it "should return Rails.logger" do
+        double_logger = double "Logger"
+        Rails.logger = double_logger
+
+        ExecutorWithoutExtension.acts_as_executor :logger => nil
+
+        @model = ExecutorWithoutExtension.new
+        @model.log.should eq double_logger
+        @model.logger.should eq double_logger
+      end
+    end
+  end
+end
