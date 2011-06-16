156f9845c5a1ec7d0754d753e77d877ac170cf5b
Phil Ostler <philostler@gmail.com>
2011-06-16 22:47:11 +0100

    Logger can now be passed in per model class. Closes #1

--- lib/acts_as_executor/executor/model/base.rb
+++ lib/acts_as_executor/executor/model/base.rb
@@ -2,12 +2,16 @@ module ActsAsExecutor
   module Executor
     module Model
       module Base
-        def acts_as_executor
+        def acts_as_executor *arguments
           send :include, ActsAsExecutor::Executor::Model::Actions
           send :include, ActsAsExecutor::Executor::Model::Associations
           send :include, ActsAsExecutor::Executor::Model::Attributes
           send :include, ActsAsExecutor::Executor::Model::Helpers
+          send :extend, ActsAsExecutor::Executor::Model::Logger
           send :include, ActsAsExecutor::Executor::Model::Validations
+
+          hash = arguments.last.is_a?(Hash) ? arguments.pop : {}
+          self.logger = hash[:logger]
 
           if ActsAsExecutor.rails_startup?
             all
