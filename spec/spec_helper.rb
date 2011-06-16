d543ec6761411b0aac72e0fced6f12d6d2b5c6c3
Phil Ostler <philostler@gmail.com>
2011-06-16 22:49:24 +0100

    Factored out database connection, schemas and models.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,6 @@
 require "acts_as_executor"
 require "acts_as_executor/version"
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "jdbcsqlite3",
-  :database => ":memory:"
-)
-
-ActiveRecord::Schema.define do
-  self.verbose = false
-
-  create_table :executors, :force => true do |t|
-    t.string  :name
-    t.string  :kind
-    t.integer :size
-  end
-end
-
-class Executor < ActiveRecord::Base
-  acts_as_executor
-end
+require "support/database_connection"
+require "support/schemas"
+require "support/models"
