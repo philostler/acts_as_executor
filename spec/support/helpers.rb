0f1c24f00c160064f9b5279bbc513dab93983de2
Phil Ostler <philostler@gmail.com>
2011-06-19 00:54:36 +0100

    Initial commit. #2

---
+++ spec/support/helpers.rb
@@ -0,0 +1,13 @@
+def double_executor_activerecord_model class_name
+  ActiveRecord::Schema.define do
+    self.verbose = false
+
+    create_table class_name.tableize, :force => true do |t|
+      t.string  :name
+      t.string  :kind
+      t.integer :size
+    end
+  end
+
+  Object.const_set class_name, Class.new(ActiveRecord::Base)
+end
