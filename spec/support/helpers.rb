af1879bd3276091bf0d40e8205ee83b0c7686522
Phil Ostler <philostler@gmail.com>
2011-06-19 01:14:26 +0100

    Executor task helper. #2

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -11,3 +11,21 @@ def double_executor_model class_name
 
   Object.const_set class_name, Class.new(ActiveRecord::Base)
 end
+
+def double_executor_task_model class_name
+  ActiveRecord::Schema.define do
+    self.verbose = false
+
+    create_table class_name.tableize, :force => true do |t|
+      t.integer  :executor_id
+      t.string   :clazz
+      t.string   :arguments
+      t.string   :schedule
+      t.integer  :start
+      t.integer  :every
+      t.string   :units
+    end
+  end
+
+  Object.const_set class_name, Class.new(ActiveRecord::Base)
+end
