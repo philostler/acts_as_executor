6ebfc3ef5c67715014c8faa13184e6fdb9e24cd5
Phil Ostler <philostler@gmail.com>
2011-06-19 01:14:49 +0100

    Define executor task models. #2

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -1,6 +1,9 @@
 double_executor_model("Executor").acts_as_executor
 double_executor_model("ExecutorWithoutExtension")
 
+double_executor_task_model("ExecutorTask").acts_as_executor_task
+double_executor_task_model("ExecutorTaskWithoutExtension")
+
 class Rails
   def self.logger
     @@logger
