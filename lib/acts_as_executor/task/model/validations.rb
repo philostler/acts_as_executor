0f05449cd18d45b8e18ce69570b02dc1dd88d4b4
Phil Ostler <philostler@gmail.com>
2011-06-24 02:51:17 +0100

    Validation class name change. #6

--- lib/acts_as_executor/task/model/validations.rb
+++ lib/acts_as_executor/task/model/validations.rb
@@ -4,7 +4,7 @@ module ActsAsExecutor
       module Validations
         def self.included base
           base.validates :executor_id, :presence => true
-          base.validates :clazz, :presence => true, :class_exists => true
+          base.validates :clazz, :presence => true, :class_exists_with_required_module => true
           base.validates :schedule, :inclusion => { :in => ActsAsExecutor::Task::Schedules::ALL }, :if => "executor and executor.schedulable?"
           base.validates :start, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }, :if => "executor and executor.schedulable?"
           base.validates :every, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1 }, :if => "executor and executor.schedulable? and schedule != ActsAsExecutor::Task::Schedules::ONE_SHOT"
