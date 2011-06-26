074529897b32bb3480e40e3e5d1d66ebac9ce8b8
Phil Ostler <philostler@gmail.com>
2011-06-26 16:04:15 +0100

    Attributes are dynamically written to the class/instance. #5

--- lib/acts_as_executor/task/clazz.rb
+++ lib/acts_as_executor/task/clazz.rb
@@ -3,8 +3,17 @@ module ActsAsExecutor
     module Clazz
       include Java::java.lang.Runnable
 
+      attr_writer :arguments
+
       private
       def run
+        if @arguments
+          @arguments.each_pair do |k, v|
+            class_eval { attr_accessor k }
+            send "#{k}=", v
+          end
+        end
+
         execute
       end
     end
