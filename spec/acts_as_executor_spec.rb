7ff6b005f12157e1628acd5fab22a78e3b33748a
Phil Ostler <philostler@gmail.com>
2011-06-27 23:45:02 +0100

    Simplified equality expression. #2

--- spec/acts_as_executor_spec.rb
+++ spec/acts_as_executor_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe ActsAsExecutor do
   describe "VERSION" do
-    specify { ActsAsExecutor::VERSION.should eq "1.0.0.rc1" }
+    specify { ActsAsExecutor::VERSION.should == "1.0.0.rc1" }
   end
 
   describe "#rails_booted?" do
