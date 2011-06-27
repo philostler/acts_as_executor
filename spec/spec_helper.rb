281068426d5462bb551f86fb02fbea0ceda37b6b
Phil Ostler <philostler@gmail.com>
2011-06-27 23:47:30 +0100

    Helper requires. #2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,7 @@ require "acts_as_executor"
 require "acts_as_executor/version"
 
 require "support/database_connection"
-require "support/helpers"
+require "support/class_helpers"
+require "support/double_helpers"
+require "support/receive_helpers"
 require "support/models"
