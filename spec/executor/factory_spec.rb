17b3a7d8472facc4f8e6118b226162e9f6054b6e
Phil Ostler <philostler@gmail.com>
2011-06-27 23:49:24 +0100

    Simplified equality expression. #2

--- spec/executor/factory_spec.rb
+++ spec/executor/factory_spec.rb
@@ -14,17 +14,17 @@ describe ActsAsExecutor::Executor::Factory do
           it "should return a ExecutorService of size 1" do
             e = ActsAsExecutor::Executor::Factory.create(ActsAsExecutor::Executor::Kinds::FIXED, 1)
             e.should be_a Java::java.util.concurrent.ExecutorService
-            e.core_pool_size.should eq 1
+            e.core_pool_size.should == 1
           end
           it "should return a ExecutorService of size 50" do
             e = ActsAsExecutor::Executor::Factory.create(ActsAsExecutor::Executor::Kinds::FIXED, 50)
             e.should be_a Java::java.util.concurrent.ExecutorService
-            e.core_pool_size.should eq 50
+            e.core_pool_size.should == 50
           end
           it "should return a ExecutorService of size 999999" do
             e = ActsAsExecutor::Executor::Factory.create(ActsAsExecutor::Executor::Kinds::FIXED, 999999)
             e.should be_a Java::java.util.concurrent.ExecutorService
-            e.core_pool_size.should eq 999999
+            e.core_pool_size.should == 999999
           end
         end
         context "when invalid" do
@@ -48,17 +48,17 @@ describe ActsAsExecutor::Executor::Factory do
           it "should return a ScheduledExecutorService of size 1" do
             e = ActsAsExecutor::Executor::Factory.create(ActsAsExecutor::Executor::Kinds::SCHEDULED, 1)
             e.should be_a Java::java.util.concurrent.ScheduledExecutorService
-            e.core_pool_size.should eq 1
+            e.core_pool_size.should == 1
           end
           it "should return a ScheduledExecutorService of size 50" do
             e = ActsAsExecutor::Executor::Factory.create(ActsAsExecutor::Executor::Kinds::SCHEDULED, 50)
             e.should be_a Java::java.util.concurrent.ScheduledExecutorService
-            e.core_pool_size.should eq 50
+            e.core_pool_size.should == 50
           end
           it "should return a ScheduledExecutorService of size 999999" do
             e = ActsAsExecutor::Executor::Factory.create(ActsAsExecutor::Executor::Kinds::SCHEDULED, 999999)
             e.should be_a Java::java.util.concurrent.ScheduledExecutorService
-            e.core_pool_size.should eq 999999
+            e.core_pool_size.should == 999999
           end
         end
         context "when invalid" do
