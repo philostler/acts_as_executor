8558f1a9b5af845cf88e1c9c2093b3844b1cee2a
Phil Ostler <philostler@gmail.com>
2011-06-27 23:49:54 +0100

    Simplified equality expression and method renames. #2

--- spec/executor/model/logging_spec.rb
+++ spec/executor/model/logging_spec.rb
@@ -4,35 +4,35 @@ describe ActsAsExecutor::Executor::Model::Logging do
   describe "#logger" do
     context "default logger" do
       it "should return Rails.logger" do
-        double_logger = double_rails_logger
+        double_logger = double_rails_logger_and_assign
 
         @model = Executor.new
-        @model.send(:log).should eq double_logger
-        @model.send(:logger).should eq double_logger
+        @model.send(:log).should == double_logger
+        @model.send(:logger).should == double_logger
       end
     end
     context "custom logger" do
       it "should return custom logger" do
         double_logger = double "Logger"
 
-        double_executor_model "ExecutorWithCustomLogger"
+        define_executor_model_class "ExecutorWithCustomLogger"
         ExecutorWithCustomLogger.acts_as_executor :logger => double_logger
 
         @model = ExecutorWithCustomLogger.new
-        @model.send(:log).should eq double_logger
-        @model.send(:logger).should eq double_logger
+        @model.send(:log).should == double_logger
+        @model.send(:logger).should == double_logger
       end
     end
     context "nil logger" do
       it "should return Rails.logger" do
-        double_logger = double_rails_logger
+        double_logger = double_rails_logger_and_assign
 
-        double_executor_model "ExecutorWithNilLogger"
+        define_executor_model_class "ExecutorWithNilLogger"
         ExecutorWithNilLogger.acts_as_executor :logger => nil
 
         @model = ExecutorWithNilLogger.new
-        @model.send(:log).should eq double_logger
-        @model.send(:logger).should eq double_logger
+        @model.send(:log).should == double_logger
+        @model.send(:logger).should == double_logger
       end
     end
   end
