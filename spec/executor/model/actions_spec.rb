5385bbb81f0aa2955846ce52528c3d3b8a509d68
Phil Ostler <philostler@gmail.com>
2011-07-04 01:25:51 +0100

    Test methods via save and not direct invoke. #2

--- spec/executor/model/actions_spec.rb
+++ spec/executor/model/actions_spec.rb
@@ -11,42 +11,43 @@ describe ActsAsExecutor::Executor::Model::Actions do
     end
     it "should be accessible via send method" do
       double_rails_logger_and_assign
-      expect { @model.send :startup }.to_not raise_error
+      expect { @model.send :startup }.to_not raise_error NoMethodError
     end
 
     context "when valid" do
       it "should create executor" do
         double_rails_logger_and_assign
         should_receive_rails_booted? true
-        @model.id = 1
 
         @model.send(:executor).should be_nil
         @model.send(:log).should_receive(:debug).with("\"Executor\" executor startup triggered")
         @model.send(:log).should_receive(:info).with("\"Executor\" executor started")
 
-        @model.send :startup
+        @model.save
 
         @model.send(:executor).should_not be_nil
       end
-      it "should not create executor when already invoked and not shutdown" do
-        double_rails_logger_and_assign
+      it "should not create executor when an executor already exists" do
         should_receive_rails_booted? true
-        @model.id = 2
 
-        @model.send :startup
+        @model.save
         executor = @model.send :executor
 
-        @model.send(:log).should_receive(:warn).with("\"Executor\" executor startup triggered but has already been started")
-
-        @model.send :startup
+        @model.save
 
         @model.send(:executor).should == executor
       end
-      it "should queue all association tasks" do
-      end
+      it "should queue all association tasks"
     end
     context "when invalid" do
       it "should not create executor" do
+        should_receive_rails_booted? true
+        @model.name = ""
+        @model.kind = ""
+
+        @model.save
+
+        @model.send(:executor).should be_nil
       end
     end
   end
