05fcc9e37ec657c32090fd9375801c81ea03009d
Phil Ostler <philostler@gmail.com>
2011-07-04 01:24:23 +0100

    Added additional 2 methods. #2

--- spec/executor/model/helpers_spec.rb
+++ spec/executor/model/helpers_spec.rb
@@ -5,6 +5,62 @@ describe ActsAsExecutor::Executor::Model::Helpers do
     @model = Executor.new
   end
 
+  describe "#can_startup?" do
+    context "when valid" do
+      it "should return true" do
+        @model.id = 1
+        @model.send :executor=, nil
+        should_receive_rails_booted? true
+
+        @model.can_startup?.should be_true
+      end
+    end
+    context "when invalid" do
+      it "should return false when executor is not nil" do
+        @model.id = 1
+        @model.send :executor=, true
+        should_receive_rails_booted? true
+
+        @model.can_startup?.should be_false
+      end
+      it "should return false when rails_booted? is false" do
+        @model.id = 1
+        @model.send :executor=, nil
+        should_receive_rails_booted? false
+
+        @model.can_startup?.should be_false
+      end
+    end
+  end
+
+  describe "#can_shutdown?" do
+    context "when valid" do
+      it "should return true" do
+        @model.id = 1
+        @model.send :executor=, true
+        should_receive_rails_booted? true
+
+        @model.can_shutdown?.should be_true
+      end
+    end
+    context "when invalid" do
+      it "should return false when executor is nil" do
+        @model.id = 1
+        @model.send :executor=, nil
+        should_receive_rails_booted? true
+
+        @model.can_shutdown?.should be_false
+      end
+      it "should return false when rails_booted? is false" do
+        @model.id = 1
+        @model.send :executor=, true
+        should_receive_rails_booted? false
+
+        @model.can_shutdown?.should be_false
+      end
+    end
+  end
+
   describe "#schedulable?" do
     context "when valid" do
       it "should return false when kind is cached" do
