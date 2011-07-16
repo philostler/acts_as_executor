fd703c2ad0d3563197fe00a9ad91327ba275ed4a
Phil Ostler <philostler@gmail.com>
2011-07-16 17:39:13 +0100

    Double for future task. #2

--- spec/support/double_helpers.rb
+++ spec/support/double_helpers.rb
@@ -4,6 +4,12 @@ def double_clazz
   double_clazz
 end
 
+def double_future_task
+  instance = double_clazz
+  double_future_task = ActsAsExecutor::Common::FutureTask.new instance, nil
+  double_future_task
+end
+
 def double_rails_logger_and_assign
   double_logger = double "Logger"
   double_logger.stub :debug
