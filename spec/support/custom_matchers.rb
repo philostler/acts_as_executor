e3d3e46d8b32df6969db470544b85b0fe67e92f2
Phil Ostler <philostler@gmail.com>
2011-07-23 13:38:16 +0100

    Initial commit. #2

---
+++ spec/support/custom_matchers.rb
@@ -0,0 +1,25 @@
+RSpec::Matchers.define :allow_public_access_for_methods do |*methods|
+  match_for_should do |object|
+    responded_to_all = true
+    methods.each do |method|
+      responded_to_all = false unless object.respond_to? method
+    end
+    responded_to_all
+  end
+
+  match_for_should_not do |object|
+    not_responded_to_all = true
+    methods.each do |method|
+      not_responded_to_all = false if object.respond_to? method
+    end
+    not_responded_to_all
+  end
+end
+
+def log_message model, operation
+  "\"" + model.name + "\" executor " + operation
+end
+
+def log_message_with_task model, doing, clazz, operation
+  "\"" + model.name + "\" executor " + doing + " task \"" + clazz.class.name + "\" with arguments \"" + clazz.arguments.inspect + "\" " + operation
+end
