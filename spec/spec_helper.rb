cd807be6849ec83655dd4fe350ef46fd1d8b65f9
Phil Ostler <philostler@gmail.com>
2011-07-23 14:04:01 +0100

    require machinist and updated support classes. #2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,9 @@
 require "acts_as_executor"
 require "acts_as_executor/version"
 
+require "machinist/active_record"
+
 require "support/database_connection"
-require "support/class_helpers"
-require "support/double_helpers"
-require "support/receive_helpers"
-require "support/models"
+require "support/classes"
+require "support/blueprints"
+require "support/custom_matchers"
