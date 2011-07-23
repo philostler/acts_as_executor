ac57bc2215d24086bec4999e7738126bd36be32b
Phil Ostler <philostler@gmail.com>
2011-07-23 13:40:15 +0100

    Initial commit. #2

---
+++ spec/support/classes.rb
@@ -0,0 +1,44 @@
+# Logger
+class Logger
+  def debug message = nil, &block; end
+  def info message = nil, &block; end
+  def warn message = nil, &block; end
+  def error message = nil, &block; end
+  def fatal message = nil, &block; end
+end
+
+# Executor
+Object.const_set "Executor", Class.new(ActiveRecord::Base)
+ActiveRecord::Schema.define do
+  self.verbose = false
+
+  create_table :executors, :force => true do |t|
+    t.string  :name
+    t.string  :kind
+    t.integer :size
+  end
+end
+Executor.acts_as_executor
+Executor.log = Logger.new
+
+# ExecutorTask
+Object.const_set "ExecutorTask", Class.new(ActiveRecord::Base)
+ActiveRecord::Schema.define do
+  self.verbose = false
+
+  create_table :executor_tasks, :force => true do |t|
+    t.integer  :executor_id
+    t.string   :clazz
+    t.string   :arguments
+    t.string   :schedule
+    t.integer  :start
+    t.integer  :every
+    t.string   :units
+  end
+end
+ExecutorTask.acts_as_executor_task
+
+# Clazz
+Object.const_set "Clazz", Class.new
+Clazz.send :extend, Machinist::Machinable
+Clazz.send :include, ActsAsExecutor::Task::Clazz
