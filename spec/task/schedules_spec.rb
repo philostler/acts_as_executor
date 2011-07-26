4ce0e61f620ea46c6c8e857d51d2cc714c373fde
Phil Ostler <philostler@gmail.com>
2011-07-26 23:46:57 +0100

    Condensed. #2

--- spec/task/schedules_spec.rb
+++ spec/task/schedules_spec.rb
@@ -1,20 +1,14 @@
 require "spec_helper"
 
 describe ActsAsExecutor::Task::Schedules do
-  describe "ONE_SHOT" do
-    specify { ActsAsExecutor::Task::Schedules::ONE_SHOT.should == "one_shot" }
-  end
-  describe "FIXED_DELAY" do
-    specify { ActsAsExecutor::Task::Schedules::FIXED_DELAY.should == "fixed_delay" }
-  end
-  describe "FIXED_RATE" do
-    specify { ActsAsExecutor::Task::Schedules::FIXED_RATE.should == "fixed_rate" }
-  end
+  it { ActsAsExecutor::Task::Schedules::ONE_SHOT.should == "one_shot" }
 
-  describe "ALL" do
-    specify { ActsAsExecutor::Task::Schedules::ALL.should include(
-      ActsAsExecutor::Task::Schedules::ONE_SHOT,
-      ActsAsExecutor::Task::Schedules::FIXED_DELAY,
-      ActsAsExecutor::Task::Schedules::FIXED_RATE) }
-  end
+  it { ActsAsExecutor::Task::Schedules::FIXED_DELAY.should == "fixed_delay" }
+
+  it { ActsAsExecutor::Task::Schedules::FIXED_RATE.should == "fixed_rate" }
+
+  it { ActsAsExecutor::Task::Schedules::ALL.should include ActsAsExecutor::Task::Schedules::ONE_SHOT,
+                                                           ActsAsExecutor::Task::Schedules::FIXED_DELAY,
+                                                           ActsAsExecutor::Task::Schedules::FIXED_RATE
+  }
 end
