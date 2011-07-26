d2df6494acaf21f6b573a754770106a443d9bac9
Phil Ostler <philostler@gmail.com>
2011-07-26 23:56:27 +0100

    Condensed. #2

--- spec/common/units_spec.rb
+++ spec/common/units_spec.rb
@@ -1,36 +1,26 @@
 require "spec_helper"
 
 describe ActsAsExecutor::Common::Units do
-  describe "NANOSECONDS" do
-    specify { ActsAsExecutor::Common::Units::NANOSECONDS.should == "nanoseconds" }
-  end
-  describe "MICROSECONDS" do
-    specify { ActsAsExecutor::Common::Units::MICROSECONDS.should == "microseconds" }
-  end
-  describe "MILLISECONDS" do
-    specify { ActsAsExecutor::Common::Units::MILLISECONDS.should == "milliseconds" }
-  end
-  describe "SECONDS" do
-    specify { ActsAsExecutor::Common::Units::SECONDS.should == "seconds" }
-  end
-  describe "MINUTES" do
-    specify { ActsAsExecutor::Common::Units::MINUTES.should == "minutes" }
-  end
-  describe "HOURS" do
-    specify { ActsAsExecutor::Common::Units::HOURS.should == "hours" }
-  end
-  describe "DAYS" do
-    specify { ActsAsExecutor::Common::Units::DAYS.should == "days" }
-  end
+  it { ActsAsExecutor::Common::Units::NANOSECONDS.should == "nanoseconds" }
 
-  describe "ALL" do
-    specify { ActsAsExecutor::Common::Units::ALL.should include(
-      ActsAsExecutor::Common::Units::NANOSECONDS,
-      ActsAsExecutor::Common::Units::MICROSECONDS,
-      ActsAsExecutor::Common::Units::MILLISECONDS,
-      ActsAsExecutor::Common::Units::SECONDS,
-      ActsAsExecutor::Common::Units::MINUTES,
-      ActsAsExecutor::Common::Units::HOURS,
-      ActsAsExecutor::Common::Units::DAYS) }
-  end
+  it { ActsAsExecutor::Common::Units::MICROSECONDS.should == "microseconds" }
+
+  it { ActsAsExecutor::Common::Units::MILLISECONDS.should == "milliseconds" }
+
+  it { ActsAsExecutor::Common::Units::SECONDS.should == "seconds" }
+
+  it { ActsAsExecutor::Common::Units::MINUTES.should == "minutes" }
+
+  it { ActsAsExecutor::Common::Units::HOURS.should == "hours" }
+
+  it { ActsAsExecutor::Common::Units::DAYS.should == "days" }
+
+  it { ActsAsExecutor::Common::Units::ALL.should include ActsAsExecutor::Common::Units::NANOSECONDS,
+                                                         ActsAsExecutor::Common::Units::MICROSECONDS,
+                                                         ActsAsExecutor::Common::Units::MILLISECONDS,
+                                                         ActsAsExecutor::Common::Units::SECONDS,
+                                                         ActsAsExecutor::Common::Units::MINUTES,
+                                                         ActsAsExecutor::Common::Units::HOURS,
+                                                         ActsAsExecutor::Common::Units::DAYS
+  }
 end
