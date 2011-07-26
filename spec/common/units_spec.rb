13ed275127abb75ac35c7d75c4ee8f5a2ec7b223
Phil Ostler <philostler@gmail.com>
2011-07-27 00:13:54 +0100

    Removed spacing. #2

--- spec/common/units_spec.rb
+++ spec/common/units_spec.rb
@@ -2,19 +2,12 @@ require "spec_helper"
 
 describe ActsAsExecutor::Common::Units do
   it { ActsAsExecutor::Common::Units::NANOSECONDS.should == "nanoseconds" }
-
   it { ActsAsExecutor::Common::Units::MICROSECONDS.should == "microseconds" }
-
   it { ActsAsExecutor::Common::Units::MILLISECONDS.should == "milliseconds" }
-
   it { ActsAsExecutor::Common::Units::SECONDS.should == "seconds" }
-
   it { ActsAsExecutor::Common::Units::MINUTES.should == "minutes" }
-
   it { ActsAsExecutor::Common::Units::HOURS.should == "hours" }
-
   it { ActsAsExecutor::Common::Units::DAYS.should == "days" }
-
   it { ActsAsExecutor::Common::Units::ALL.should include ActsAsExecutor::Common::Units::NANOSECONDS,
                                                          ActsAsExecutor::Common::Units::MICROSECONDS,
                                                          ActsAsExecutor::Common::Units::MILLISECONDS,
--- spec/task/schedules_spec.rb
+++ spec/task/schedules_spec.rb
@@ -2,11 +2,8 @@ require "spec_helper"
 
 describe ActsAsExecutor::Task::Schedules do
   it { ActsAsExecutor::Task::Schedules::ONE_SHOT.should == "one_shot" }
-
   it { ActsAsExecutor::Task::Schedules::FIXED_DELAY.should == "fixed_delay" }
-
   it { ActsAsExecutor::Task::Schedules::FIXED_RATE.should == "fixed_rate" }
-
   it { ActsAsExecutor::Task::Schedules::ALL.should include ActsAsExecutor::Task::Schedules::ONE_SHOT,
                                                            ActsAsExecutor::Task::Schedules::FIXED_DELAY,
                                                            ActsAsExecutor::Task::Schedules::FIXED_RATE
