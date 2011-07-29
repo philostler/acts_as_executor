2fcb23c143fd9eb17f6c19b525004a670a0477a3
Phil Ostler <philostler@gmail.com>
2011-07-29 16:34:02 +0100

    Modify requires for new executor and task structure.

--- lib/acts_as_executor.rb
+++ lib/acts_as_executor.rb
@@ -7,28 +7,21 @@ require "acts_as_executor/common/units"
 require "acts_as_executor/executor/factory"
 require "acts_as_executor/executor/kinds"
 
-require "acts_as_executor/executor/model/actions"
-require "acts_as_executor/executor/model/associations"
-require "acts_as_executor/executor/model/attributes"
-require "acts_as_executor/executor/model/base"
-require "acts_as_executor/executor/model/helpers"
-require "acts_as_executor/executor/model/logging"
-require "acts_as_executor/executor/model/validations"
+require "acts_as_executor/executor/model/class_methods"
+require "acts_as_executor/executor/model/instance_methods"
+require "acts_as_executor/executor/model/instance_support_methods"
 
 require "acts_as_executor/task/clazz"
 require "acts_as_executor/task/schedules"
 
-require "acts_as_executor/task/model/actions"
-require "acts_as_executor/task/model/associations"
-require "acts_as_executor/task/model/attributes"
-require "acts_as_executor/task/model/base"
-require "acts_as_executor/task/model/helpers"
-require "acts_as_executor/task/model/validations"
+require "acts_as_executor/task/model/class_methods"
+require "acts_as_executor/task/model/instance_methods"
+require "acts_as_executor/task/model/instance_support_methods"
 
 require "acts_as_executor/validators/class_exists_with_required_module_validator"
 
-ActiveRecord::Base.send :extend, ActsAsExecutor::Executor::Model::Base
-ActiveRecord::Base.send :extend, ActsAsExecutor::Task::Model::Base
+ActiveRecord::Base.send :extend, ActsAsExecutor::Executor::Model::ClassMethods
+ActiveRecord::Base.send :extend, ActsAsExecutor::Task::Model::ClassMethods
 
 module ActsAsExecutor
   def self.rails_booted?
