054c2d6db3c49c734dccfb5868d71d8076583d4a
Phil Ostler <philostler@gmail.com>
2011-08-05 00:08:38 +0100

    Uses log message method and syntax clean up. Closes #2

--- lib/acts_as_executor/task/model/instance_methods.rb
+++ lib/acts_as_executor/task/model/instance_methods.rb
@@ -16,7 +16,7 @@ module ActsAsExecutor
 
           # Validations
           base.validates :executor_id, :presence => true
-          base.validates :clazz, :presence => true, :class_exists_with_required_module => true
+          base.validates :clazz, :presence => true, :class_exists => true, :class_includes => { :includes => ActsAsExecutor::Task::Clazz }
           base.validates :schedule, :inclusion => { :in => ActsAsExecutor::Task::Schedules::ALL }, :if => "executor and executor.schedulable?"
           base.validates :start, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }, :if => "executor and executor.schedulable?"
           base.validates :every, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1 }, :if => "executor and executor.schedulable? and schedule != ActsAsExecutor::Task::Schedules::ONE_SHOT"
@@ -26,14 +26,14 @@ module ActsAsExecutor
         private
         def enqueue
           begin
-            self.log.debug "\"" + executor.name + "\" executor instantiating task \"" + clazz + "\" with arguments \"" + arguments.inspect + "\" for execution"
+            executor.send(:log).debug log_message "instantiating", "for enqueuing"
 
             instance = instantiate clazz, arguments
 
             self.future = executor.send(:execute, instance, schedule, start, every, units)
-            self.future.done_handler = method :done_handler
+            future.done_handler = method :done_handler
           rescue Exception => exception
-            executor.send(:log).error "\"" + executor.name + "\" executor instantiating task \"" + clazz + "\" with arguments \"" + arguments.inspect + "\" experienced an exception error. " + exception
+            executor.send(:log).error log_message "instantiating", "encountered an unexpected exception. " + exception
           end
         end
 
@@ -44,22 +44,18 @@ module ActsAsExecutor
           instance
         end
 
-        def cancel
-          if self.future.cancel true
-            # success
-          else
-            # failed
-          end
-
-          self.future = nil
-        end
-
         def done_handler
           destroy
         end
 
         def uncaught_exception_handler exception
-          executor.send(:log).error "\"" + executor.name + "\" executor executing task \"" + clazz + "\" experienced an uncaught exception. " + exception
+          executor.send(:log).error log_message "executing", "encountered an uncaught exception. " + exception
+        end
+
+        def cancel
+          future.cancel true
+
+          self.future = nil
         end
       end
     end
