4ab7e7c2ac59c1d63c025d167530cf8b6b5b0835
Phil Ostler <philostler@gmail.com>
2011-08-05 01:22:00 +0100

    Invoke new on constant.

--- lib/acts_as_executor/validators/class_includes_validator.rb
+++ lib/acts_as_executor/validators/class_includes_validator.rb
@@ -1,7 +1,7 @@
 class ClassIncludesValidator < ActiveModel::EachValidator
   def validate_each record, attribute, value
     begin
-      throw TypeError unless Object.const_get(value).kind_of? options[:includes]
+      throw TypeError unless Object.const_get(value).new.kind_of? options[:includes]
     rescue NameError
     rescue TypeError
       record.errors[attribute] << "does not include " + options[:includes].to_s
