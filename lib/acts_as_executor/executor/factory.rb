3d3d096785b1079744065f92d41d046793a9fd21
Phil Ostler <philostler@gmail.com>
2011-08-21 03:12:10 +0100

    New implementation decides on the fly what executor type to create. #11

--- lib/acts_as_executor/executor/factory.rb
+++ lib/acts_as_executor/executor/factory.rb
@@ -1,45 +1,26 @@
 module ActsAsExecutor
   module Executor
     class Factory
-      def self.create kind, size = nil
+      def self.create max_tasks, schedulable
         executor = nil
-        case kind
-          when ActsAsExecutor::Executor::Kinds::CACHED
-            executor = create_cached
-          when ActsAsExecutor::Executor::Kinds::FIXED
-            executor = create_fixed size
-          when ActsAsExecutor::Executor::Kinds::SINGLE
-            executor = create_single
-          when ActsAsExecutor::Executor::Kinds::SCHEDULED
-            executor = create_scheduled size
-          when ActsAsExecutor::Executor::Kinds::SINGLE_SCHEDULED
-            executor = create_single_scheduled
+        if schedulable
+          if max_tasks == nil
+            # No Cached Scheduled Implementation
+          elsif max_tasks >  1
+            executor = Java::java.util.concurrent.Executors.new_scheduled_thread_pool max_tasks.to_i # Scheduled
+          else
+            executor = Java::java.util.concurrent.Executors.new_single_thread_scheduled_executor # Single Scheduled
+          end
+        else
+          if max_tasks == nil
+            executor = Java::java.util.concurrent.Executors.new_cached_thread_pool # Cached
+          elsif max_tasks > 1
+            executor = Java::java.util.concurrent.Executors.new_fixed_thread_pool max_tasks.to_i # Fixed
+          else
+            executor = Java::java.util.concurrent.Executors.new_single_thread_executor # Single
+          end
         end
         executor
-      end
-
-      def self.create_cached
-        Java::java.util.concurrent.Executors.new_cached_thread_pool
-      end
-
-      def self.create_fixed size
-        raise TypeError, "size cannot be nil" unless size
-        raise ArgumentError, "size must be larger than 0" unless size.to_i > 0
-        Java::java.util.concurrent.Executors.new_fixed_thread_pool size.to_i
-      end
-
-      def self.create_single
-        Java::java.util.concurrent.Executors.new_single_thread_executor
-      end
-
-      def self.create_scheduled size
-          raise TypeError, "size cannot be nil" unless size
-          raise ArgumentError, "size must be larger than 0" unless size.to_i > 0
-          Java::java.util.concurrent.Executors.new_scheduled_thread_pool size.to_i
-      end
-
-      def self.create_single_scheduled
-        Java::java.util.concurrent.Executors.new_single_thread_scheduled_executor
       end
     end
   end
--- spec/executor/factory_spec.rb
+++ spec/executor/factory_spec.rb
@@ -2,76 +2,30 @@ require "spec_helper"
 
 describe ActsAsExecutor::Executor::Factory do
   describe "#create" do
-    it "should invoke matching create method for kind" do
-      ActsAsExecutor::Executor::Factory.should_receive :create_cached
-
-      ActsAsExecutor::Executor::Factory.create ActsAsExecutor::Executor::Kinds::CACHED
-    end
-
-    context "when no kind is given" do
-      it "should return nil" do
-        ActsAsExecutor::Executor::Factory.create(nil).should be_nil
-      end
-    end
-  end
-
-  describe "#create_cached" do
-    it "should return an executor" do
-      ActsAsExecutor::Executor::Factory.create_cached.should be_a Java::java.util.concurrent.ExecutorService
-    end
-  end
-
-  describe "#create_fixed" do
-    it "should return an executor" do
-      executor = ActsAsExecutor::Executor::Factory.create_fixed 5
-
-      executor.should be_a Java::java.util.concurrent.ExecutorService
-      executor.core_pool_size.should == 5
-    end
-
-    context "when no size is given" do
-      it "should raise a type error" do
-        expect { ActsAsExecutor::Executor::Factory.create_fixed nil }.to raise_error TypeError, "size cannot be nil"
-      end
-    end
-
-    context "when given size is less than or equal to zero" do
-      it "should raise an argument error" do
-        expect { ActsAsExecutor::Executor::Factory.create_fixed 0 }.to raise_error ArgumentError, "size must be larger than 0"
-      end
-    end
-  end
-
-  describe "#create_single" do
-    it "should return an executor" do
-      ActsAsExecutor::Executor::Factory.create_single.should be_a Java::java.util.concurrent.ExecutorService
-    end
-  end
-
-  describe "#create_scheduled" do
-    it "should return an executor" do
-      executor = ActsAsExecutor::Executor::Factory.create_scheduled 5
+    it "should return a scheduled executor" do
+      executor = ActsAsExecutor::Executor::Factory.create 5, true
 
       executor.should be_a Java::java.util.concurrent.ScheduledExecutorService
       executor.core_pool_size.should == 5
     end
 
-    context "when no size is given" do
-      it "should raise a type error" do
-        expect { ActsAsExecutor::Executor::Factory.create_scheduled nil }.to raise_error TypeError, "size cannot be nil"
-      end
+    it "should return a single scheduled executor" do
+      ActsAsExecutor::Executor::Factory.create(1, true).should be_a Java::java.util.concurrent.ScheduledExecutorService
     end
 
-    context "when given size is less than or equal to zero" do
-      it "should raise an argument error" do
-        expect { ActsAsExecutor::Executor::Factory.create_scheduled 0 }.to raise_error ArgumentError, "size must be larger than 0"
-      end
+    it "should return a cached executor" do
+      ActsAsExecutor::Executor::Factory.create(nil, nil).should be_a Java::java.util.concurrent.ExecutorService
     end
-  end
 
-  describe "#create_single_scheduled" do
-    it "should return an executor" do
-      ActsAsExecutor::Executor::Factory.create_single_scheduled.should be_a Java::java.util.concurrent.ScheduledExecutorService
+    it "should return a fixed executor" do
+      executor = ActsAsExecutor::Executor::Factory.create 5, nil
+
+      executor.should be_a Java::java.util.concurrent.ExecutorService
+      executor.core_pool_size.should == 5
+    end
+
+    it "should return a single executor" do
+      ActsAsExecutor::Executor::Factory.create(1, nil).should be_a Java::java.util.concurrent.ExecutorService
     end
   end
 end
