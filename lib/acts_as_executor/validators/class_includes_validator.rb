192e1208f6ea25c0d708da3680a02935cfcc549a
Phil Ostler <philostler@gmail.com>
2011-09-18 19:57:11 +0100

    Removed unnecessary throw and work around Ruby 1.9.2-p136 bug.

--- lib/acts_as_executor/validators/class_includes_validator.rb
+++ lib/acts_as_executor/validators/class_includes_validator.rb
@@ -1,10 +1,11 @@
 class ClassIncludesValidator < ActiveModel::EachValidator
   def validate_each record, attribute, value
     begin
-      throw TypeError unless Object.const_get(value).new.kind_of? options[:includes]
+      unless Object.const_get(value).new.kind_of? options[:includes]
+        record.errors[attribute] << "does not include " + options[:includes].to_s
+      end
     rescue NameError
-    rescue TypeError
-      record.errors[attribute] << "does not include " + options[:includes].to_s
+      # Ignore error if class does not exist.
     end
   end
 end
