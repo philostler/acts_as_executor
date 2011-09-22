6ce10a01e537f5435034d0c8c1ce9e974227df23
Phil Ostler <philostler@gmail.com>
2011-09-23 00:09:08 +0100

    Renamed of clazz to executable. #19

--- lib/acts_as_executor/common/instance_support_methods.rb
+++ lib/acts_as_executor/common/instance_support_methods.rb
@@ -6,9 +6,9 @@ module ActsAsExecutor
         "\"" + executor_name + "\" " + statement
       end
 
-      def log_message executor_name, doing, task_id, clazz_name, message = ""
+      def log_message executor_name, doing, task_id, executable_name, message = ""
         if message.kind_of? Hash then message = "with \"" + message.inspect + "\"" end 
-        "\"" + executor_name + "\" " + doing + " \"" + task_id + "#" + clazz_name + "\" " + message
+        "\"" + executor_name + "\" " + doing + " \"" + task_id + "#" + executable_name + "\" " + message
       end
     end
   end
