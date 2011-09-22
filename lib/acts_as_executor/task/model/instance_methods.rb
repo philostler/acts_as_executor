3117b495498add3a6234dd328b981129ca8bfe80
Phil Ostler <philostler@gmail.com>
2011-09-23 00:10:39 +0100

    Rename of clazz to executable. #19

--- lib/acts_as_executor/task/model/instance_methods.rb
+++ lib/acts_as_executor/task/model/instance_methods.rb
@@ -16,7 +16,7 @@ module ActsAsExecutor
 
           # Validations
           base.validates :executor_id, :presence => true
-          base.validates :clazz, :presence => true, :class_exists => true, :class_includes => { :includes => ActsAsExecutor::Task::Clazz }
+          base.validates :executable, :presence => true, :class_exists => true, :class_subclasses => { :superclass => ActsAsExecutor::Task::Executable }
           base.validates :schedule, :inclusion => { :in => ActsAsExecutor::Task::Schedules::ALL }, :if => "executor and executor.schedulable?"
           base.validates :start, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }, :if => "executor and executor.schedulable?"
           base.validates :every, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1 }, :if => "executor and executor.schedulable? and schedule != ActsAsExecutor::Task::Schedules::ONE_SHOT"
@@ -26,14 +26,14 @@ module ActsAsExecutor
         private
         def enqueue
           begin
-            executor.send(:log).debug log_message executor.name, "creating", id.to_s, clazz, arguments
+            executor.send(:log).debug log_message executor.name, "creating", id.to_s, executable, arguments
 
-            instance = instantiate clazz, arguments
+            instance = instantiate executable, arguments
 
             self.future = executor.send(:execute, instance, id.to_s, schedule, start, every, units)
             future.done_handler = method :done_handler
           rescue Exception => exception
-            executor.send(:log).error log_message executor.name, "creating", id.to_s, clazz, "encountered an unexpected exception. " + exception.to_s
+            executor.send(:log).error log_message executor.name, "creating", id.to_s, executable, "encountered an unexpected exception. " + exception.to_s
           end
         end
 
@@ -45,12 +45,12 @@ module ActsAsExecutor
         end
 
         def done_handler
-          executor.send(:log).debug log_message executor.name, "completed", id.to_s, clazz
+          executor.send(:log).debug log_message executor.name, "completed", id.to_s, executable
           destroy
         end
 
         def uncaught_exception_handler exception
-          executor.send(:log).error log_message executor.name, "executing", id.to_s, clazz, "encountered an uncaught exception. " + exception.to_s
+          executor.send(:log).error log_message executor.name, "executing", id.to_s, executable, "encountered an uncaught exception. " + exception.to_s
         end
 
         def cancel
