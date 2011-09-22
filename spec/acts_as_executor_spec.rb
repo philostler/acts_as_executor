8641ff66d285e9ea08b4e9d85f805bc0d992f26c
Phil Ostler <philostler@gmail.com>
2011-09-23 00:04:18 +0100

    Rolled to 1.0.0.rc2.

--- spec/acts_as_executor_spec.rb
+++ spec/acts_as_executor_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe ActsAsExecutor do
-	it { ActsAsExecutor::VERSION.should == "1.0.0" }
+	it { ActsAsExecutor::VERSION.should == "1.0.0.rc2" }
 
   describe "#rails_initialized?" do
     context "when rails initialized" do
