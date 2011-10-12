b5ca55b5a5aaac2153463164933b3f8949e70d83
Phil Ostler <philostler@gmail.com>
2011-10-12 23:39:30 +0100

    Only create two types of executor. #25

--- lib/acts_as_executor/executor/factory.rb
+++ lib/acts_as_executor/executor/factory.rb
@@ -1,24 +1,12 @@
 module ActsAsExecutor
   module Executor
     class Factory
-      def self.create max_tasks, schedulable
+      def self.create limit
         executor = nil
-        if schedulable
-          if max_tasks == nil
-            # No Cached Scheduled Implementation
-          elsif max_tasks >  1
-            executor = Java::java.util.concurrent.Executors.new_scheduled_thread_pool max_tasks.to_i # Scheduled
-          else
-            executor = Java::java.util.concurrent.Executors.new_single_thread_scheduled_executor # Single Scheduled
-          end
+        if limit.nil?
+          executor = Java::java.util.concurrent.Executors.new_cached_thread_pool
         else
-          if max_tasks == nil
-            executor = Java::java.util.concurrent.Executors.new_cached_thread_pool # Cached
-          elsif max_tasks > 1
-            executor = Java::java.util.concurrent.Executors.new_fixed_thread_pool max_tasks.to_i # Fixed
-          else
-            executor = Java::java.util.concurrent.Executors.new_single_thread_executor # Single
-          end
+          executor = Java::java.util.concurrent.Executors.new_scheduled_thread_pool limit.to_i
         end
         executor
       end
