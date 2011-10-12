b63a8f1b8ab91ee617b471c021e852bea886e545
Phil Ostler <philostler@gmail.com>
2011-10-12 23:20:59 +0100

    Removed schedules model. #23

--- lib/acts_as_executor.rb
+++ lib/acts_as_executor.rb
@@ -13,7 +13,6 @@ require "acts_as_executor/executor/model/instance_methods"
 require "acts_as_executor/executor/model/instance_support_methods"
 
 require "acts_as_executor/task/executable"
-require "acts_as_executor/task/schedules"
 
 require "acts_as_executor/task/model/class_methods"
 require "acts_as_executor/task/model/instance_methods"
