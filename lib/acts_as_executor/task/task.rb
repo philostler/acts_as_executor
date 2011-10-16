5b0df8f1034c6b72d190c568842ca5a93a544abf
Phil Ostler <philostler@gmail.com>
2011-10-17 00:58:11 +0100

    Added before and after execution handlers. #25

--- lib/acts_as_executor/task/task.rb
+++ lib/acts_as_executor/task/task.rb
@@ -4,11 +4,12 @@ module ActsAsExecutor
       include Java::java.lang.Runnable
 
       attr_accessor :arguments
-      attr_writer :uncaught_exception_handler
+      attr_writer :before_execute_handler, :after_execute_handler, :uncaught_exception_handler
 
       private
       def run
         begin
+          @before_execute_handler.call if @before_execute_handler
           if @arguments
             @arguments.each_pair do |key, value|
               class_eval { attr_accessor key } unless respond_to? key
@@ -19,6 +20,8 @@ module ActsAsExecutor
           execute
         rescue Exception => exception
           @uncaught_exception_handler.call exception if @uncaught_exception_handler
+        ensure
+          @after_execute_handler.call if @after_execute_handler
         end
       end
     end
