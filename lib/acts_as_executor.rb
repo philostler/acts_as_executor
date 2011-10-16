953fdfe0d34734e1dcad4904fd56c14e6177475b
Phil Ostler <philostler@gmail.com>
2011-10-17 00:52:22 +0100

    Removed as code refactored out. #25

--- lib/acts_as_executor.rb
+++ lib/acts_as_executor.rb
@@ -3,7 +3,6 @@ require "active_record"
 
 require "acts_as_executor/version"
 
-require "acts_as_executor/common/future_task"
 require "acts_as_executor/common/instance_support_methods"
 
 require "acts_as_executor/executor/model/class_methods"
