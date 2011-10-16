f972b5c2e92f72ed85a74ff11a1d1c5d9a2c7372
Phil Ostler <philostler@gmail.com>
2011-10-17 00:59:38 +0100

    Rework and handler now defined in Procs. #25

--- lib/acts_as_executor/task/model/instance_methods.rb
+++ lib/acts_as_executor/task/model/instance_methods.rb
@@ -16,45 +16,49 @@ module ActsAsExecutor
 
           # Validations
           base.validates :executor_id, :presence => true
-          base.validates :executable, :presence => true, :class_exists => true, :class_subclasses => { :superclass => ActsAsExecutor::Task::Executable }
-          base.validates :schedule, :inclusion => { :in => ActsAsExecutor::Task::Schedules::ALL }, :if => "executor and executor.schedulable?"
-          base.validates :start, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }, :if => "executor and executor.schedulable?"
-          base.validates :every, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1 }, :if => "executor and executor.schedulable? and schedule != ActsAsExecutor::Task::Schedules::ONE_SHOT"
+          base.validates :task, :presence => true, :class_exists => true, :class_subclasses => { :superclass => ActsAsExecutor::Task::Task }
+          base.validates :start, :presence => true, :if => "!every.nil?"
+          base.validates :start, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }, :allow_nil => true
+          base.validates :every, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1 }, :allow_nil => true
         end
 
         private
         def enqueue
           begin
-            executor.send(:log).debug log_message executor.name, "creating", id.to_s, executable, arguments
+            executor.send(:log).debug log_message executor.name, "creating", id.to_s, task, arguments
 
-            instance = instantiate executable, arguments
+            instance = instantiate task, arguments
 
-            self.future = executor.send(:execute, instance, id.to_s, schedule, start, every)
-            future.done_handler = method :done_handler unless executor.send(:schedulable?)
+            self.future = executor.send(:execute, instance, id.to_s, start, every, every_strict)
           rescue RuntimeError => exception
-            executor.send(:log).error log_message executor.name, "creating", id.to_s, executable, "encountered an unexpected exception. " + exception.to_s
+            executor.send(:log).error log_message executor.name, "creating", id.to_s, task, "encountered an unexpected exception. " + exception.to_s
           end
         end
 
         def instantiate class_name, arguments
           instance = Object.const_get(class_name).new
           instance.arguments = arguments
-          instance.uncaught_exception_handler = method :uncaught_exception_handler
+
+          instance.before_execute_handler = Proc.new {
+            executor.send(:log).debug log_message executor.name, "started", id.to_s, task
+            started_at = Time.now
+          }
+
+          instance.after_execute_handler = Proc.new {
+            executor.send(:log).debug log_message executor.name, "completed", id.to_s, task
+            completed_at = Time.now
+            save
+          }
+
+          instance.uncaught_exception_handler = Proc.new {
+            executor.send(:log).error log_message executor.name, "executing", id.to_s, task, "encountered an uncaught exception. " + exception.to_s
+          }
+
           instance
-        end
-
-        def done_handler
-          executor.send(:log).debug log_message executor.name, "completed", id.to_s, executable
-          destroy
-        end
-
-        def uncaught_exception_handler exception
-          executor.send(:log).error log_message executor.name, "executing", id.to_s, executable, "encountered an uncaught exception. " + exception.to_s
         end
 
         def cancel
           future.cancel true
-
           self.future = nil
         end
       end
